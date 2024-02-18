ad767d89a898cd7241aa41819f1a322adcde15b1
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-02-18 16:39:12 +0600

    auth guard added to sales route

--- src/modules/sales/sales.route.ts
+++ src/modules/sales/sales.route.ts
@@ -4,16 +4,40 @@ import { SalesController } from "./sales.controller";
 
 const router = Router();
 
-router.post("/sell-product", auth(), SalesController.sellProduct);
+router.post(
+  "/sell-product",
+  auth("superAdmin", "seller"),
+  SalesController.sellProduct,
+);
 
-router.get("/history", auth(), SalesController.getHistory);
+router.get(
+  "/history",
+  auth("superAdmin", "branchManager", "seller"),
+  SalesController.getHistory,
+);
 
-router.get("/history-daily", auth(), SalesController.getHistoryDaily);
+router.get(
+  "/history-daily",
+  auth("superAdmin", "branchManager", "seller"),
+  SalesController.getHistoryDaily,
+);
 
-router.get("/history-weekly", auth(), SalesController.getHistoryWeekly);
+router.get(
+  "/history-weekly",
+  auth("superAdmin", "branchManager", "seller"),
+  SalesController.getHistoryWeekly,
+);
 
-router.get("/history-monthly", auth(), SalesController.getHistoryMonthly);
+router.get(
+  "/history-monthly",
+  auth("superAdmin", "branchManager", "seller"),
+  SalesController.getHistoryMonthly,
+);
 
-router.get("/history-yearly", auth(), SalesController.getHistoryYearly);
+router.get(
+  "/history-yearly",
+  auth("superAdmin", "branchManager", "seller"),
+  SalesController.getHistoryYearly,
+);
 
 export const SalesRoute = router;
